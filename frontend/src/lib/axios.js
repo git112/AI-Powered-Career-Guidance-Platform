e3dd4f7e5fc5d22e04518ca1e917e4b42ff0089b
23IT019_DABHI KEVAL <23it019@charusat.edu.in>
2025-03-20 23:16:07 +0530

    modify

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -9,30 +9,7 @@ const api = axios.create({
   },
 });
 
-// // Add a request interceptor to include the auth token in requests
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('token');
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
 
-// // Add a response interceptor to handle common errors
-// api.interceptors.response.use(
-//   response => response,
-//   error => {
-//     console.error('API Error:', error.response || error);
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default api;
 
 
 // Request interceptor for API calls
